2460b10fcd09b0cd4459d3a3a9e883f7382e7c83
FCDevs <fernandocis@gmail.com>
2021-12-08 03:56:24 -0300

    correciones a mensaje mqtt: id sensor y estado como String

--- main.cpp
+++ main.cpp
@@ -1,20 +1,14 @@
 #include <Arduino.h>
-#include <WiFi.h> //<WiFi101.h>
+#include <WiFi.h>
 #include <MQTT.h>
 
 const char ssid[] = "NxGroup";
 const char pass[] = "cisneros530";
+String IDS = "NO IDENTIFICADO";
+String estado = "CORRECTO";
 
 WiFiClient net;
 MQTTClient client;
-
-
-const int freq =5000; // freq for PWM
-const int ledChannel = 0; // channel pwm
-const int resolution = 10; // resolution 8,10,12,15
-
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
 
 long duration;
 float distanceCm;
@@ -34,87 +28,68 @@ void connect() {
     Serial.print(".");
     delay(1000);
   }
-
   Serial.println("\nconnected!");
-
   client.subscribe("led");
-
   // client.unsubscribe("/hello");
 }
 
 void messageReceived(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
-  // set the brightness of pin 2:
   if (topic=="led") {
     if (payload=="on") digitalWrite(26, HIGH);
     if (payload=="off") digitalWrite(26, LOW);
   }
-
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
 }
 
 float medicion(){
-  // Clears the trigPin
-    digitalWrite(12, LOW);
+    digitalWrite(12, LOW); // Clears the trigPin
     delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(12, HIGH);
+    digitalWrite(12, HIGH);// Sets the trigPin on HIGH state for 10 micro seconds
     delayMicroseconds(10);
     digitalWrite(12, LOW);
     duration = pulseIn(13, HIGH);
     distanceCm = duration * SOUND_SPEED/2;
+    if (distanceCm < 20.0)
+    {
+      estado = "BAJA";
+    }
+    else
+    {
+      if(distanceCm > 80.0)
+      {
+        estado = "ALTA";
+      }
+      else
+      {
+        estado = "NORMAL";
+      }
+    }
     return distanceCm;
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(25,OUTPUT); // LED1
-  pinMode(26,OUTPUT); // LED2
 
-  pinMode(34,INPUT);  //pulsadores
-  pinMode(35,INPUT);  //pulsadores
+  pinMode(26,OUTPUT); // LED2
   pinMode(13,INPUT);  // ECHO
   pinMode(12,OUTPUT); // pulse
-  ledcSetup(ledChannel,freq,resolution);
-  ledcAttachPin(25,ledChannel); // LED1 como PWM
-  //ledcAttachPin(26,ledChannel);
-  //ledcAttachPin(27,ledChannel);
 
   WiFi.begin(ssid, pass);
-
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
-  // by Arduino. You need to set the IP address directly.
-  //
-  // MQTT brokers usually use port 8883 for secure connections.
   client.begin("192.168.0.125", 1883, net);
   client.onMessage(messageReceived);
-
   connect();
- 
 }
 
 void loop() {
-  
-  client.loop(); // Si o si tiene que estar en el loop
-
-  if (!client.connected()) {
-    connect();
+  client.loop();              // Si o si tiene que estar en el loop
+  if (!client.connected())    
+  {
+    connect();                // si se desconecta, intento conectar
   }
 
   if ((millis() - lastTime) > timerDelay) {
     lastTime = millis();
-    String mensaje ="{\"temperature\":"+String(medicion())+",\"ID_SENSOR\":2,\"state\":5}";
+    String mensaje ="{\"temperature\":"+String(medicion())+",\"ID_SENSOR\":\""+String(IDS)+"\",\"state\":\""+String(estado)+"\"}";
     client.publish("temperatura", mensaje);
-  }
- 
-  
-    
-    
-    
-  
-    
+  }  
 }
